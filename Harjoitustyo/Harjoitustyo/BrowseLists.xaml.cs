e5e2fe88e47a889c27ef77029b99df660cb51d26
sebastian <laaksonen.sebastian@gmail.com>
2017-04-04 16:39:42 +0300

    Suunnittellut sivut lisätty ja pari testiä

--- Harjoitustyo/Harjoitustyo/BrowseLists.xaml.cs
+++ Harjoitustyo/Harjoitustyo/BrowseLists.xaml.cs
@@ -13,33 +13,31 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Harjoitustyo
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class BrowseLists : Page
     {
-        public MainPage()
+        public BrowseLists()
         {
             this.InitializeComponent();
         }
 
-        private void AddItem_button_Click(object sender, RoutedEventArgs e)
+        private void ListFiles_button_Click(object sender, RoutedEventArgs e)
         {
-            this.Frame.Navigate(typeof(AddNew));
+            
+        
+            
+            
         }
 
-        private void PrintList_button_Click(object sender, RoutedEventArgs e)
+        private void GoBack_button_Click(object sender, RoutedEventArgs e)
         {
-            this.Frame.Navigate(typeof(PrintList));
-        }
-
-        private void BrowseLists_button_Click(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(BrowseLists));
+            this.Frame.Navigate(typeof(MainPage));
         }
     }
 }
